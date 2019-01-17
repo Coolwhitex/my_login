da286a3b271017229dd7133221d02494299a2ff2
Coolwhitex <2648447509@qq.com>
2019-01-17 16:43:12 +0800

    基础版本完

--- login/views.py
+++ login/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.hashers import make_password, check_password
 from . import models
 
 # Create your views here.
+import hashlib
 
 
 def index(request):
@@ -22,7 +24,8 @@ def login(request):
         # print(username, password)
         user = models.User.objects.filter(name=username).first()
         if user:
-            if user.password == password:
+            if _hash_password(password) == user.hash_password:
+            # if user.password == password:
                 content['message'] = '登录成功'
                 # 服务器设置sessionid和其他用户信息。sessionid(服务器给访问他的浏览器的身份证)自动生成的
                 request.session['is_login'] = True
@@ -46,14 +49,28 @@ def register(request):
         username = request.POST.get('username')
         password = request.POST.get('password')
         email = request.POST.get('email')
-        if not (username.strip() and password.strip() and email.strip()):
-            print("某个字段为空")
-            return render(request,'/register', context={'message': '某个字段为空'})
-        if len(username)>20 or  len(password) >20:
-            print("用户名和密码过长")
+        print(username, password, email)
+        # if not (username.strip() and password.strip() and email.strip()):
+        #     print("某个字段为空")
+        #     return render(request, '/register', context={'message': '某个字段为空'})
+        # if len(username)>20 or len(password) >20:
+        #     print("用户名和密码过长")
+        # redirect('/login/')
 
         # 写数据库
-        redirect('/login/')
+        user = models.User.objects.filter(email=email).first()
+        if user:
+            '用户已存在'
+            return render(request, 'register.html', context= {'message': '用户已注册'})
+        hash_password = _hash_password(password)
+        try:
+            user1 = models.User(name=username, password=password, email=email, hash_password=hash_password)
+            user1.save()
+            print('保存成功')
+            return redirect('/login/')
+        except Exception as e:
+            print(f'保存失败{e}')
+            return redirect('/register/')
 
 
 def logout(request):
@@ -63,6 +80,22 @@ def logout(request):
     return redirect('/index/')
 
 
+def _hash_password(password):
+    """哈希加密用户注册密码"""
+    sha = hashlib.sha256()
+    sha.update(password.encode(encoding='utf-8'))
+    return sha.hexdigest()
+
+"""
+def _hash_password(password):
+    哈希加密用户注册密码 加盐版
+    salt = ''
+    for i in range(4)
+        salt +=str(random)
+    sha = hashlib.sha256()
+    sha.update(password.encode(encoding='utf-8'))
+    return sha.hexdigest()
+ """
 
 
   # 查询数据库         # 相当于 'select * from login_user where name=%s and password=%s' %s(username, password)
